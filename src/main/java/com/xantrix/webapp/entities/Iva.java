42e04ca5e58cb229434955bfce6c3dee39e048df
EdoardoBaral <edoardo@baral.it>
2025-04-22 23:11:21 +0200

    [Lezione 44] Refactoring entity Iva

--- src/main/java/com/xantrix/webapp/entities/Iva.java
+++ src/main/java/com/xantrix/webapp/entities/Iva.java
@@ -1,27 +1,18 @@
 package com.xantrix.webapp.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 @Table(name = "IVA")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Iva 
-{
+@Data
+@RequiredArgsConstructor
+public class Iva {
+	
 	@Id
 	@Column(name = "IDIVA")
 	private int idIva;
@@ -34,5 +25,4 @@ public class Iva
 	
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "iva")
 	private Set<Articoli> articoli = new HashSet<>();
-	
 }
